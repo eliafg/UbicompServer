362bac753a2432b5b31bd266ebd1c8e9719d4220
Eliaf Yahir <eliaf.garcia@uabc.edu.mx>
2023-08-09 01:57:53 -0700

    Initial Commit

--- app.js
+++ app.js
@@ -52,6 +52,29 @@ app.post('/incidentes', async (req, res) => {
   }
 });
 
+app.get('/latest', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('incidentes');
+    const collection = database.collection('incidentes');
+
+    const documents = await collection
+      .find()
+      .sort({ _id: -1 }) // Sort in reverse order by _id (assuming _id is a timestamp)
+      .limit(10) // Limit to the last 10 documents
+      .toArray();
+
+    res.json(documents);
+  } catch (error) {
+    console.error('Error retrieving documents:', error);
+    res.status(500).send('Error retrieving documents');
+  }
+});
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
